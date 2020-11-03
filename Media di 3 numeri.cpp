5bae40cac9423d00efdf43d7a5ff655aa313bff9
Giulia-Giugie <73116499+Giulia-Giugie@users.noreply.github.com>
2020-11-03 14:56:51 +0100

    Add files via upload

---
+++ Media di 3 numeri.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int numero1,numero2,numero3;
+	cout << "Inserisci il numero: ";
+	cin >> numero1;
+	cout << "Inserisci il numero: ";
+	cin >> numero2;
+	cout << "Inserisci il numero: ";
+	cin >> numero3;
+	cout<< "La media e':"<<(numero1+numero2+numero3)/3;
+	return 0;
+
+}
+
